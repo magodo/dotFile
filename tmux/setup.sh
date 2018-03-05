40155bd3183d2949a26c9cb73cd278dfd4b17fd2
magodo <wztdyl@sina.com>
2018-03-05 12:04:59 +0800

    set EDITOR to vim when setup oh-my-tmux

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -20,6 +20,26 @@ Install tmux config file to $HOME based on your tmux version
 EOF
 }
 
+line_in_file()
+{
+    target_line="$1"
+    file="$2"
+ 
+    if [[ ! -f $file ]]; then
+        # create a new file if not existed
+        echo "$target_line" >> "$file"
+    else
+        while IFS= read -r line; do
+            # use exact matching (instead of pattern matching)
+            [[ "$line" = "$target_line" ]] && { is_find=; break; }
+        done < $file
+ 
+        if [[ -z ${is_find+x} ]]; then
+            echo "$target_line" >> $file
+        fi
+    fi
+}
+
 setup_oh_my_tmux()
 {
     #[[ -L $HOME/.tmux ]] && rm $HOME/.tmux # in case link follow link
@@ -29,6 +49,9 @@ setup_oh_my_tmux()
 
     # use the customized one instead of the official provided one
     ln -s -f $OH_MY_TMUX_DIR/.tmux.conf.local $HOME/.tmux.conf.local
+
+    # make tmux in vim mode
+    line_in_file "export EDITOR=$(which vim)" $HOME/.bashrc
 }
 
 setup_simple()
