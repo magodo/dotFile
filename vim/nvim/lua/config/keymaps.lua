7b132e7b8612d946176702840bba352419028025
magodo <wztwcy@gmail.com>
2026-04-08 21:56:29 +1000

    nvim: update

--- vim/nvim/lua/config/keymaps.lua
+++ vim/nvim/lua/config/keymaps.lua
@@ -17,3 +17,18 @@ keymap("n", "<leader><leader>", "<c-^>")
 
 -- Working Directory
 keymap("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>")
+
+-- LSP
+-- These GLOBAL keymaps are created unconditionally when Nvim starts:
+-- "gra" (Normal and Visual mode) is mapped to vim.lsp.buf.code_action()
+-- "gri" is mapped to vim.lsp.buf.implementation()
+-- "grn" is mapped to vim.lsp.buf.rename()
+-- "grr" is mapped to vim.lsp.buf.references()
+-- "grt" is mapped to vim.lsp.buf.type_definition()
+-- "grx" is mapped to vim.lsp.codelens.run()
+-- "gO" is mapped to vim.lsp.buf.document_symbol()
+-- CTRL-S (Insert mode) is mapped to vim.lsp.buf.signature_help()
+-- v_an and v_in fall back to LSP vim.lsp.buf.selection_range() if treesitter is not active.
+-- gx handles textDocument/documentLink. Example: with gopls, invoking gx on "os" in this Go code will open documentation externally:
+keymap('n', '[g', vim.diagnostic.goto_prev, {desc = "Previous diagnostic"})
+keymap('n', ']g', vim.diagnostic.goto_next, {desc = "Next diagnostic"})
