fe0719938035622cdfa78a1967e01eabda0b34e7
magodo <wztwcy@gmail.com>
2026-04-10 14:05:58 +1000

    nvim: keymap update

--- vim/nvim/lua/config/keymaps.lua
+++ vim/nvim/lua/config/keymaps.lua
@@ -30,6 +30,18 @@ keymap("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>")
 -- CTRL-S (Insert mode) is mapped to vim.lsp.buf.signature_help()
 -- v_an and v_in fall back to LSP vim.lsp.buf.selection_range() if treesitter is not active.
 -- gx handles textDocument/documentLink. Example: with gopls, invoking gx on "os" in this Go code will open documentation externally:
-keymap('n', '<C-k>', vim.lsp.buf.hover, {desc = "Hoever"})
-keymap('n', '[g', vim.diagnostic.goto_prev, {desc = "Previous diagnostic"})
-keymap('n', ']g', vim.diagnostic.goto_next, {desc = "Next diagnostic"})
+keymap('n', 'gD', vim.lsp.buf.declaration)
+keymap('n', 'gd', vim.lsp.buf.definition)
+keymap('n', 'gi', vim.lsp.buf.implementation)
+keymap('n', 'gr', vim.lsp.buf.references)
+keymap('n', '<leader>D', vim.lsp.buf.type_definition)
+keymap('n', '<leader>rn', vim.lsp.buf.rename)
+keymap('n', '<leader>ca', vim.lsp.buf.code_action)
+keymap('n', '<leader>f', vim.lsp.buf.format)
+keymap('n', 'K', vim.lsp.buf.hover)
+keymap('n', '<C-k>', vim.lsp.buf.signature_help)
+
+-- Diagnostics
+keymap('n', '<leader>e', vim.diagnostic.open_float)
+keymap('n', '[g', vim.diagnostic.goto_prev)
+keymap('n', ']g', vim.diagnostic.goto_next)
