d4c1c01511deb51a9a181482b96693a36ad81bf6
magodo <wztwcy@gmail.com>
2026-05-23 09:31:36 +1000

    blink: use C-e to show completions

--- vim/nvim/lua/plugins/lsp/blink.cmp.lua
+++ vim/nvim/lua/plugins/lsp/blink.cmp.lua
@@ -25,7 +25,10 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'enter' },
+    keymap = {
+      preset = 'enter',
+      ['<C-e>'] = { 'show' },
+    },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
