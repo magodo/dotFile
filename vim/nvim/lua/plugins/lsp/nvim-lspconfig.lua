e029500793c196c6b41d9134c13f32dd3e3fd35a
magodo <wztwcy@gmail.com>
2026-05-22 16:24:29 +1000

    Add az-rs lsp

--- vim/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ vim/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,3 +1,16 @@
 return {
     "neovim/nvim-lspconfig",
+    config = function()
+        vim.filetype.add({
+            extension = {
+                az = "az",
+            },
+        })
+
+        vim.lsp.config("azure", {
+            filetypes = { "az" },
+            cmd = { "/home/magodo/github/az-rs/target/debug/azure", "lsp" },
+        })
+        vim.lsp.enable("azure")
+    end,
 }
